711d0eeed49ca620e7862f6c45d8bf864b617cac
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-11-22 15:51:06 -0400

    Added Links to Buttons

--- src/Footer.js
+++ src/Footer.js
@@ -5,15 +5,15 @@ export default function Footer() {
   return (
     <div className="Footer">
       <a href="https://twitter.com/nayobaez">
-        <i className="fa-brands fa-square-twitter"></i>
+        <i className="fa-brands fa-square-twitter" target="_blank"></i>
       </a>
-      <a href="https://www.facebook.com/NAYOBAEZ/">
+      <a href="https://www.facebook.com/NAYOBAEZ/" target="_blank">
         <i className="fa-brands fa-square-facebook"></i>
       </a>
-      <a href="https://www.instagram.com/nayobaez/">
+      <a href="https://www.instagram.com/nayobaez/" target="_blank">
         <i className="fa-brands fa-square-instagram"></i>
       </a>
-      <a href="https://github.com/NayoBaez/">
+      <a href="https://github.com/NayoBaez/" target="_blank">
         <i className="fa-brands fa-square-github"></i>
       </a>
     </div>
--- src/Info.js
+++ src/Info.js
@@ -12,13 +12,16 @@ export default function Info() {
       <h1>Naomi Bàez</h1>
       <h2>Frontend Developer</h2>
       <h3>naomi.website</h3>
-      <button className="email--button">
-        <i className="fa-solid fa-envelope"></i> Email
-      </button>
-      <button className="linkedin--button">
-        {" "}
-        <i className="fa-brands fa-linkedin"></i> LinkedIn
-      </button>
+      <a href="mailto:info@nayobaez.com" target="_blank">
+        <button className="email--button">
+          <i className="fa-solid fa-envelope"></i> Email
+        </button>
+      </a>
+      <a href="https://www.linkedin.com/in/nayobaezfeliz/" target="_blank">
+        <button className="linkedin--button">
+          <i className="fa-brands fa-linkedin"></i> LinkedIn
+        </button>
+      </a>
       <About />
       <Interests />
       <Footer />
