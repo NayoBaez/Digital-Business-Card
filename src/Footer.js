d3dad323d10b9138a068e4e147092d79a6b569cb
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-11-22 15:59:36 -0400

    Added rel="noreferrer" to fix deployment error

--- src/Footer.js
+++ src/Footer.js
@@ -5,15 +5,27 @@ export default function Footer() {
   return (
     <div className="Footer">
       <a href="https://twitter.com/nayobaez">
-        <i className="fa-brands fa-square-twitter" target="_blank"></i>
+        <i
+          className="fa-brands fa-square-twitter"
+          target="_blank"
+          rel="noreferrer"
+        ></i>
       </a>
-      <a href="https://www.facebook.com/NAYOBAEZ/" target="_blank">
+      <a
+        href="https://www.facebook.com/NAYOBAEZ/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <i className="fa-brands fa-square-facebook"></i>
       </a>
-      <a href="https://www.instagram.com/nayobaez/" target="_blank">
+      <a
+        href="https://www.instagram.com/nayobaez/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <i className="fa-brands fa-square-instagram"></i>
       </a>
-      <a href="https://github.com/NayoBaez/" target="_blank">
+      <a href="https://github.com/NayoBaez/" target="_blank" rel="noreferrer">
         <i className="fa-brands fa-square-github"></i>
       </a>
     </div>
